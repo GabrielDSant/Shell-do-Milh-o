39433290bdb5cec49bf4bc5f8d5b3899f06b70f2
GabrielDSant <gabrielbiel1597533@gmail.com>
2022-02-07 09:59:35 -0300

    Formatei o pc socorro

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 from doctest import testfile
 import random
 import json
-import time
 import cowsay
 
 varbanner = ['1', '2', '3']
 temas = ['Red', 'Blue']
+
 
 #iniciar o jogo com banner 
 def banners():
@@ -20,7 +20,6 @@ def menu():
     print("Bem-vindo ao shell do milhão :)\nPara o jogo iniciar escolha o tema da vez.\n \n|Red| para perguntas focadas no red team.\n|Blue| para perguntas focadas no blue team.")
     Escolha_do_tema = input("Tema escolhido: ")
     if Escolha_do_tema in temas:
-        print("tema encontrado")
         tema(Escolha_do_tema)
     else:
         print("tema não encontrado")
@@ -30,7 +29,7 @@ def menu():
 def tema(arquivo):
     with open ('./temas/'+arquivo+'.json', 'r') as temajson:
         opçoesjson = json.load(temajson)
-        print(opçoesjson["2"]["questao"])
+        print(cowsay.cow(opçoesjson[random.choice(["1", "2"])]["questao"]))
     #forma de chamar bagulho do json é aqui em cima em to com sono
 
 #mostrar perguntas ligadas ao tema do arquivo json
@@ -40,7 +39,7 @@ def pergunta(questão):
 def resposta(escolha):
     pass
 
-#Verificar resposta
+#Verificar resposta"""""
 def corretor():
     pass
 
